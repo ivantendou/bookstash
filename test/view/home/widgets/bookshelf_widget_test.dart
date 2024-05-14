6818288d2b17f4e33d8ea4c01b0b4111d5ecf928
Muhamad Ivan Fadillah <ivanfadillah153@gmail.com>
2024-05-14 10:14:01 +0700

    Update test

--- test/view/home/widgets/bookshelf_widget_test.dart
+++ test/view/home/widgets/bookshelf_widget_test.dart
@@ -1,7 +1,4 @@
 import 'package:bookstash/view/home/widgets/bookshelf_widget.dart';
-import 'package:bookstash/view/home/widgets/fiction_books_carousel_widget.dart';
-import 'package:bookstash/view/home/widgets/history_books_carousel_widget.dart';
-import 'package:bookstash/view/home/widgets/programming_books_carousel_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
@@ -27,8 +24,5 @@ void main() {
     expect(find.text('New Fiction Books'), findsOneWidget);
     expect(find.text('New History Books'), findsOneWidget);
     expect(find.text('New Programming Books'), findsOneWidget);
-    expect(find.byType(FictionBooksCarouselWidget), findsOneWidget);
-    expect(find.byType(HistoryBooksCarouselWidget), findsOneWidget);
-    expect(find.byType(ProgrammingBooksCarouselWidget), findsOneWidget);
   });
 }
